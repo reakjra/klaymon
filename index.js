82a16104133b1db6700ea308ebf8d8bd2407aa7d
kayedaa <mattia43211@gmail.com>
2021-10-22 21:05:10 +0200

    aaaaaaaa

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const DJSVersion = require('./package.json').dependencies['discord.js'] || require('./package.json').devDependencies['discord.js'];
 
-	
-
 		module.exports = {
 		TicTacToe : require('./src/v13/TicTacToe'),
 		Reverse : require('./src/v13/Reverse'),
@@ -10,7 +8,6 @@ const DJSVersion = require('./package.json').dependencies['discord.js'] || requi
 		Urban : require('./src/v13/Urban'),
 		RoadRace : require('./src/v13/RoadRace'),
 		RandomHexColor : require('./src/v13/RandomHexColor'),
-		chatBot : require('./src/v13/chatBot'),
 		Hangman : require('./src/v13/Hangman')
 	
 	};
